caaddfbec749571104a95446eac3b4244ef8cc68
MayankJain97 <161249207+MayankJain97@users.noreply.github.com>
2025-02-23 14:10:24 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/MayankJain97/port-folio"
                 target="_blank"
                 className="fork-btn-inner"
               >
